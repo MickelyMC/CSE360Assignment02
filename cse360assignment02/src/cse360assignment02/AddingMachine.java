35d341bc34e4bac1d0496cf2babe18e267c928d6
Ian Kelsey <ikelsey@asu.edu>
2020-10-05 19:33:20 -0700

    Initial methods and commentary for CSE360 Assignment 02

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,76 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * 
+ * */
+
+package cse360assignment02;
+
+
+/**
+ * <h1>AddingMachine</h1>
+ * The AddingMachine program implements basic add and
+ * subtract calculator functions.
+ * 
+ * @author Ian Kelsey
+ * @version 1.0
+ * @since 2020-10-05
+*/
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * Constructor
+  */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  
+  /**
+  * Returns total
+ */
+  public int getTotal () {
+    return 0;
+  }
+  
+  
+  /**
+  * Add method
+  * @param value First parameter to add method
+ */  
+  public void add (int value) {
+  }
+
+  
+  /**
+  * Subtract method
+  * @param value First parameter to subtract method
+ */
+  public void subtract (int value) {
+  }
+
+  
+  /**
+  * toString method
+ */
+  public String toString () {
+    return "";
+  }
+
+  
+  /**
+  * clear method
+ */
+  public void clear() {
+  }
+}
+
