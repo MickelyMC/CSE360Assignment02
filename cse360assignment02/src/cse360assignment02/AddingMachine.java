ca35d72aab2f3fa3093405c1e66efa80d9844ed7
Ian Kelsey <ikelsey@asu.edu>
2020-10-05 20:50:38 -0700

    Updated code to have functionality, small test program (main) included in comments. Updated comments to reflect functionality.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -8,59 +8,97 @@
  * @version 1.0
  * @since 2020-10-05
  * */
-
 package cse360assignment02;
+import java.util.ArrayList;
 
 /**
  * <h1>AddingMachine</h1>
  * The AddingMachine program implements basic add and
  * subtract calculator functions.
- * 
- * @author Ian Kelsey
- * @version 1.0
- * @since 2020-10-05
 */
 public class AddingMachine {
-  private int total;
+	
+	//integer total value
+	private int total;
+	private ArrayList<String> memory = new ArrayList<String>();
   
   /**
    * Constructor
-   * --Test comment change--
   */
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+	  clear();
+	  memory.add("myCalculator.set (0);");
+	  //total = 0;  // not needed - included for clarity
+	  //add '0' to memory
   }
   
   
   /**
-  * Returns total
+  * Returns total value
  */
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   
   /**
-  * Add method
-  * @param value First parameter to add method
+  * Add method adds parameter to the total
+  * @param value Integer value to ad to the total
  */  
   public void add (int value) {
+	  total += value;
+	  //add 'value' and operation to memory
+	  memory.add("myCalculator.add (" + value + ");");
   }
-
+  
   
   /**
-  * Subtract method
-  * @param value First parameter to subtract method
+  * Subtract method subtracts parameter from the total
+  * @param value Integer value to subtract from the total
  */
   public void subtract (int value) {
+	  total -= value;
+	  //add 'value' and operation to memory
+	  memory.add("myCalculator.subtract (" + value + ");");
   }
 
   
   /**
-  * toString method
+  * toString method, returns a string representation of the operations used
  */
   public String toString () {
-    return "";
+	  String toReturn = "";
+	  
+	  //print out memory
+	  //for(String j : memory) {
+	  //System.out.println(j);
+	  //}
+	  
+	  if (memory.size() == 0) {
+		  return "No Operations";
+	  }
+	  
+	  
+	  //Read and format memory for return String
+	  for(String i : memory) {
+		  if (i.contains("add")) {
+			  //toReturn = toReturn + "+ " + i.charAt(18) + " "; <-- No support for double digits
+			  toReturn = toReturn + "+ " + i.subSequence(18, i.indexOf(')')) + " ";
+			  
+		  }
+		  else if (i.contains("subtract")) {
+			  //toReturn = toReturn + "- " + i.charAt(23) + " "; <-- No support for double digits
+			  toReturn = toReturn + "- " + i.subSequence(23, i.indexOf(')')) + " ";
+			  
+		  }
+		  else if (i.contains("set")) {
+			  toReturn = toReturn + "0 ";
+		  }
+	  }
+
+	
+	  return toReturn;
+	  //return operations memory
   }
 
   
@@ -68,6 +106,52 @@ public class AddingMachine {
   * clear method
  */
   public void clear() {
+	  memory.clear();
   }
+  
+  
+  // Simple test program
+  public static void main(String[] args) {
+	  AddingMachine calc = new AddingMachine();
+	  
+	  System.out.println(calc.getTotal());
+	  
+	  calc.add(4);
+	  calc.add(17);
+	  calc.subtract(-3);
+	  calc.subtract(13);
+	  
+	  System.out.println(calc.toString());
+	  System.out.println(calc.getTotal());
+	  
+	  calc.clear();
+	  System.out.println(calc.toString());
+	  
+  }
+  
+  
+  
 }
+/*
+ 
+A history of the transactions must be kept to be returned by the toString method. The history should start from the initial 0 value to the last input.
 
+ The clear method should clear your "memory".
+ 
+Use the normal operators for the operations (+, -).
+
+There is no limit on the size of the String returned.
+
+
+
+The format of the String should separate all operations and values with a space. For example:
+myCalculator.add (4); 
+myCalculator.subtract (2); 
+myCalculator.add(5);
+
+then the toString method should return
+0 + 4 – 2 + 5
+
+Add other variables as needed to the class. Any additional variables must be private. Make changes to the other methods if needed.
+Update all documentations as needed
+*/
